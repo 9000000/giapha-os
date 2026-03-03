f127626b031e885e394eadb409b52005b9cd00e7
9000000 <hai90000001@gmail.com>
2026-03-03 22:23:13 +0700

    up

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -105,7 +105,7 @@ export default function LoginPage() {
               router.refresh();
             } else {
               setSuccessMessage(
-                "Đăng ký thành công! Quý khách vui lòng liên hệ trực tiếp ố điện thoại/Zalo: 0376939688 hoặc Email : [EMAIL_ADDRESS] của ban quản trị để yêu cầu kích hoạt tài khoản.",
+                "Đăng ký thành công! Quý khách vui lòng liên hệ trực tiếp số điện thoại/Zalo: 0376939688 hoặc Email : nongthanhtuan88@gmail.com của ban quản trị để yêu cầu kích hoạt tài khoản.",
               );
               setIsLogin(true); // Switch back to login view
               setConfirmPassword(""); // clear confirm password

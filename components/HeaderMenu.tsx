0d4f3f2ca2238cc5c7d2ef146555c4ad7ed16034
9000000 <hai90000001@gmail.com>
2026-03-05 22:36:54 +0700

    up

--- components/HeaderMenu.tsx
+++ components/HeaderMenu.tsx
@@ -20,7 +20,7 @@ import LogoutButton from "./LogoutButton";
 import { useUser } from "./UserProvider";
 
 export default function HeaderMenu() {
-  const { user } = useUser();
+  const { user, isAdmin } = useUser();
   const userEmail = user?.email;
   const [isOpen, setIsOpen] = useState(false);
   const menuRef = useRef<HTMLDivElement>(null);
